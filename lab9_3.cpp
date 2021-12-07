a414d2b5882a805891760263853c6da49645c7d7
0639Quercus <thsomsits.brie@gmail.com>
2021-12-07 16:08:18 +0700

    lab9_3.cpp completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -9,3 +9,46 @@
 "ONE-NIGHT-STAND"
 "MARRIED"
 */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    int age, height, wealth;
+    string status;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age <= 20) {
+        cout << "Enter your height: ";
+        cin >> height;
+
+        if (height < 160) status = "UNFRIEND";
+
+        else if (height < 175) status = "FRIEND";
+
+        else {
+            cout << "Enter your property: ";
+            cin >> wealth;
+
+            if (wealth > 69e6) status = "MARRIED";
+            else status = "ONE-NIGHT-STAND";
+        }
+
+    } 
+    
+    else if (age < 30) {
+        cout << "Enter your property: ";
+        cin >> wealth;
+
+        if (wealth > 10e6) status = "BEST FRIEND";
+        else status = "UNFRIEND";
+    } 
+    
+    else status = "UNFRIEND";
+
+    cout << "Your status = " << status;
+}
